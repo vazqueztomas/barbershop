4aa3c362f920f22f46fb419e3989d4a6f12a564b
Tomas Vazquez <vazquezt2018@gmail.com>
2025-01-10 16:38:17 -0300

    refactor(database): streamline database connection logic
    Remove commented-out code and improve clarity in the database connection setup. Ensure the MongoDB client is initialized correctly and confirm successful connection with a ping command.

--- barbershop/database/database.py
+++ barbershop/database/database.py
@@ -2,27 +2,10 @@ import os
 from dotenv import load_dotenv
 
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 load_dotenv()
 
-# MONGO_URI = os.getenv("MONGODB_URL")
-# print(MONGO_URI)
-
-# def database_connection():
-#     try:
-#         client = MongoClient(MONGO_URI)
-#         db = client["barbershop"]
-#         haircuts_db = db["haircuts"]
-#         return haircuts_db
-#     except Exception as e:
-#         print(f"An error occurred while connecting to the database: {e}")
-#         return NotImplementedError
-    
-# haircuts_collection = database_connection()
-
 uri = os.getenv("MONGODB_URL")
-client = MongoClient(uri)
-print(client)
+client = MongoClient(uri) # type: ignore
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
