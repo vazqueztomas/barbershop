1660cc3fbd12aad69c6384aebc6f3ec71496410f
vazqueztomas <vazquezt2018@gmail.com>
2025-01-17 13:57:45 +0000

    refactor(database): remove connection ping print statement
    Eliminate the print statement that confirms the MongoDB connection to reduce unnecessary console output.
    Add a noqa comment to suppress performance warnings in the haircuts retrieval function.

--- barbershop/database/database.py
+++ barbershop/database/database.py
@@ -9,8 +9,6 @@ uri = os.getenv("MONGODB_URL")
 client = MongoClient(uri)  # type: ignore
 try:
     client.admin.command("ping")
-    print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
 
 haircuts_collection = client["barbershop"]["haircuts"]
