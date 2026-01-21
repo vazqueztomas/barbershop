c4c087dbc533d9b9e41d85ec02b69a33f538a687
Tomas Vazquez <vazquezt2018@gmail.com>
2026-01-21 19:48:20 -0300

    fix(cors): update allowed origins for CORS middleware

--- barbershop/app.py
+++ barbershop/app.py
@@ -1,25 +1,11 @@
 import loguru
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from .database import create_connection
 from .routes import haircuts_router
 
 logger = loguru.logger
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://barbershop-frontend-o0ncu5w1r-vazqueztomas-projects.vercel.app",
-        "https://barbershop-plum.vercel.app",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(haircuts_router)
 
