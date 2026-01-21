d965dea0a1599acd359c9652b53419f4a91b19ed
Tomas Vazquez <vazquezt2018@gmail.com>
2026-01-21 20:05:38 -0300

    fix(cors): add CORS middleware to handle preflight requests

--- barbershop/app.py
+++ barbershop/app.py
@@ -1,5 +1,5 @@
 import loguru
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from .database import create_connection
@@ -15,6 +15,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
 
 app.include_router(haircuts_router)
 
