27b2483011a5151042c83bbe43af5416175c8151
Tomas Vazquez <vazquezt2018@gmail.com>
2026-01-20 22:00:21 -0300

    fix(config): fix backend deploy

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
-from barbershop.app import app
+import loguru
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = app
+from barbershop.database import create_connection
+from barbershop.routes import haircuts_router
+
+logger = loguru.logger
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(haircuts_router)
+
+
+@app.get("/")
+def read_root():
+    create_connection("testing.db")
+    return {"Barbershop API": "OK"}
